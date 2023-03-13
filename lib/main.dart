8e871bf1e7f1ef27ee47517477528fe2eac9b47a
awencheww <cherwenmolins@gmail.com>
2023-03-13 16:04:25 +0800

    create src/github_summary.dart to show user's repo and assigned_issue and pull request

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:github/github.dart';
+import 'package:window_to_front/window_to_front.dart';
+
 import 'github_oauth_credentials.dart';
 import 'src/github_login.dart';
-import 'package:github/github.dart';
+import 'src/github_summary.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +16,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Github Client',
+      title: 'GitHub Client',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'GithubClient'),
+      home: const MyHomePage(title: 'GitHub Client'),
     );
   }
-} //MyApp
+}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
@@ -33,35 +35,24 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GithubLoginWidget(
       builder: (context, httpClient) {
-        return FutureBuilder<CurrentUser>(
-            future: viewerDetail(httpClient.credentials.accessToken),
-            builder: (context, snapshot) {
-              var ss = snapshot.hasData
-                  ? 'Hello ${snapshot.data!.login}!'
-                  : 'Retrieving viewer login details...';
-              return Scaffold(
-                appBar: AppBar(
-                  title: Text(title),
-                  elevation: 2,
-                ),
-                body: Center(
-                  child: Text(
-                    snapshot.hasData
-                        ? 'Hello ${snapshot.data!.login}!'
-                        : 'Retrieving viewer login details...',
-                  ),
-                ),
-              );
-            });
+        WindowToFront.activate();
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+            elevation: 2,
+          ),
+          body: GitHubSummary(
+            gitHub: _getGitHub(httpClient.credentials.accessToken),
+          ),
+        );
       },
       githubClientId: githubClientId,
       githubClientSecret: githubClientSecret,
       githubScopes: githubScopes,
     );
   }
-} //MyHomePage
+}
 
-Future<CurrentUser> viewerDetail(String accessToken) async {
-  final gitHub = GitHub(auth: Authentication.withToken(accessToken));
-  return gitHub.users.getCurrentUser();
+GitHub _getGitHub(String accessToken) {
+  return GitHub(auth: Authentication.withToken(accessToken));
 }
