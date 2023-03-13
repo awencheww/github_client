afa8f641334b2058d9502a00fb05e0ce952ee83c
awencheww <cherwenmolins@gmail.com>
2023-03-13 09:36:37 +0800

    add api auth from github to login user

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '9662b9d1bcdd854f96c3';
+const githubClientSecret = 'ef74c2c17e3fca72de96db5fbc55c9c46ae754fb';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
